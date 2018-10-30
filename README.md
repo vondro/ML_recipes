c204ca4551d12b621c652cc6aad761b3327456cd
Vondro <ondra.klein@gmail.com>
2018-10-30 10:44:22 +0100

    working Hello World

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Learning machine learning based on Google's machine learning recipes on youtube
+
+scikit-learn is required. Installation:
+
+$ pip3 install -U sklearn
+
+> Possible source of problems - using pip for python2.7 to install libraries - these are not compatible with Python3.
--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,22 @@
-import sklearn
+from sklearn import tree
 
-# oranges - bumpy=0, heavier
-# apples - smooth=1, lighter
+# features
+# oranges - heavier, bumpy=0
+# apples - lighter, smooth=1
 
 # features = [[140, 'smooth'], [130, 'smooth'], [150, 'bumpy'], [170, 'bumpy']]
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 
+# labels
+# apple=0, orange=1
+labels = [0, 0, 1, 1]
+
+# train the classifier - we will use decision tree
+# classifier = box of rules
+clf = tree.DecisionTreeClassifier()
+
+# fit - synonym to "find patterns in data"
+clf = clf.fit(features, labels)
+
+# this should be an orange
+print(clf.predict([[150, 0]]))
